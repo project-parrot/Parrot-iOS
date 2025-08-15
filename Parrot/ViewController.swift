60ca2a397678a33db0a9f0abcef831a57dc00b1e
jeungHunLee <ksjs1111@gmail.com>
2025-08-15 18:12:49 +0900

    Initial commit

---
+++ Parrot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parrot
+//
+//  Created by 이정훈 on 8/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
